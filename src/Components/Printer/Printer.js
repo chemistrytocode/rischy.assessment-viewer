a735b7f3b973fcf6e762410a97d9a2a1ec4e2790
Paul Ayling <paul.ayling@bglgroup.co.uk>
2022-03-31 17:49:47 +0100

    OU - Added print functionality to button, used css to break pages

--- src/Components/Printer/Printer.js
+++ src/Components/Printer/Printer.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Message, Button } from 'semantic-ui-react'
 
-const Printer = () => (
-    <>
-        <Message
-            attached
-            icon="print"
-            header='Want to print off this risk assessment?'
-            content='Click on Print button below'
-        />
-        <Button color='orange' content="Print" icon='print' size="massive" fluid />
-    </>
-);
+const Printer = () => {
+    const handleOnClick = () => window.print();
+
+    return (
+        <>
+            <Message
+                attached
+                icon="print"
+                header='Want to print off this risk assessment?'
+                content='Click on Print button below'
+            />
+            <Button color='orange' content="Print" icon='print' size="massive" fluid onClick={handleOnClick} />
+        </>
+    );
+}
 
 export default Printer;
