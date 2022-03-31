e204150f2c6273dd8da5eb2fc387c81fda2b6ef0
Paul Ayling <paul.ayling@bglgroup.co.uk>
2022-03-31 16:46:49 +0100

    OU - Better hazard table nonbreaking spaces and headers

--- src/Components/HazardTable/HazardTable.js
+++ src/Components/HazardTable/HazardTable.js
@@ -7,12 +7,13 @@ import SvgImage from '../SvgImage/SvgImage';
 
 const HazardTable = ({ hazardChemicals }) => {
   const generateTableRows = (chemical) => {
-    const { name, state, concentration, hazard, comment } = chemical;
-    const chemicalNote = state ? state : concentration;
+    const { name, state, hazard, comment } = chemical;
+
+    const nonBreakingName = name.replace(/ /g, '\u00a0')
 
     return (
       <Table.Row>
-        <Table.Cell>{name} ({chemicalNote})</Table.Cell>
+        <Table.Cell textAlign='center'><Header as="h3">{nonBreakingName}</Header>({state})</Table.Cell>
         <Table.Cell textAlign='center'>{generateHazardCell(hazard)}</Table.Cell>
         <Table.Cell>{comment}</Table.Cell>
       </Table.Row>
@@ -25,12 +26,12 @@ const HazardTable = ({ hazardChemicals }) => {
     <>
       <Header as='h1'>Risk Assessment</Header>
       <Label color='orange' pointing="below" size="large">The following risk assessment was generated from your provided chemicals:</Label>
-      <Table celled selectable striped>
+      <Table celled selectable striped color="orange">
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>Chemical</Table.HeaderCell>
-            <Table.HeaderCell textAlign='center'>Hazard</Table.HeaderCell>
-            <Table.HeaderCell>Comments</Table.HeaderCell>
+          <Table.HeaderCell textAlign='center'><Header as="h2">Chemical</Header></Table.HeaderCell>
+            <Table.HeaderCell textAlign='center'><Header as="h2">Hazard</Header></Table.HeaderCell>
+            <Table.HeaderCell><Header as="h2">Comments</Header></Table.HeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
